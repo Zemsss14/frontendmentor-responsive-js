393dacc49a5f1a672fd0e3bdbb8967f196c5c37b
Zemsss14 <Zemoce00@gmail.com>
2023-09-25 21:44:41 +0200

    Update

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,28 +1,11 @@
+// Initialization of the is_open var
+var is_open_feature = is_open_company = is_open_menu = is_open_feature_mob = is_open_company_mob = false;
+
+
+// feature block wrapper for pc
 const feature = document.querySelector(".features");
-const company = document.querySelector(".company");
-const open_menu_icon_mobile = document.querySelector(".menu-icon-mobile");
-const close_menu_icon_mobile = document.querySelector(".close-menu-img");
-const feature_mobile = document.querySelector(".mobile-feature");
-const wrapper_sub_mobile = document.querySelector("#feature-nav-mobile");
 const wrapper_feature = document.querySelector("#feature");
-const wrapper_company = document.querySelector("#company");
-const wrapper_menu = document.querySelector("#mobile-menu");
-const mobile_nav_container = document.querySelector(".mobile-nav-container");
-
-var is_open_feature, is_open_company, is_open_menu, is_open_feature_mob = false;
-
 feature.addEventListener("click", open_close_feature);
-company.addEventListener("click", open_close_company);
-open_menu_icon_mobile.addEventListener("click", open_close_mobile_menu);
-close_menu_icon_mobile.addEventListener("click", open_close_mobile_menu);
-feature_mobile.addEventListener("click", open_close_feature_mobile);
-
-function check_close_open(class_list){
-    console.log(class_list)
-    class_list.array.forEach(element => {
-        console.log(element)
-    }) 
-};
 
 function open_close_feature(){
     if(is_open_feature==false){
@@ -33,6 +16,12 @@ function open_close_feature(){
     is_open_feature = false; 
     }
 };
+
+
+// company block wrapper for pc
+const company = document.querySelector(".company");
+const wrapper_company = document.querySelector("#company");
+company.addEventListener("click", open_close_company);
 
 function open_close_company(){
     if(is_open_company==false){
@@ -45,8 +34,16 @@ function open_close_company(){
 };
 
 
+// Opening/closing mobile navigation bar
+// open state
+const open_menu_icon_mobile = document.querySelector(".menu-icon-mobile");
+const wrapper_menu = document.querySelector("#mobile-menu");
+open_menu_icon_mobile.addEventListener("click", open_close_mobile_menu);
+// close state
+const close_menu_icon_mobile = document.querySelector(".close-menu-img");
+close_menu_icon_mobile.addEventListener("click", open_close_mobile_menu);
+
 function open_close_mobile_menu(){
-    console.log(is_open_menu)
     if(is_open_menu == false){
         wrapper_menu.classList.add("open")
         is_open_menu = true
@@ -56,13 +53,46 @@ function open_close_mobile_menu(){
     }
 };
 
+
+// Opening/closing sub-navigation bar for feature categorie
+const feature_mobile = document.querySelector("#mobile-feature");
+const wrapper_feature_sub_mobile = document.querySelector("#feature-nav-mobile");
+const arrow_up_feat_mob = document.querySelector("#arrow-up-feat-mob")
+const arrow_down_feat_mob = document.querySelector("#arrow-down-feat-mob")
+feature_mobile.addEventListener("click", open_close_feature_mobile);
+
 function open_close_feature_mobile(){
     if(is_open_feature_mob == false){
-        wrapper_sub_mobile.classList.add("open")
+        wrapper_feature_sub_mobile.classList.add("open")
+        arrow_up_feat_mob.classList.add("open")
+        arrow_down_feat_mob.classList.remove("open")
         is_open_feature_mob = true
     }else{
-        wrapper_sub_mobile.classList.remove("open")
+        wrapper_feature_sub_mobile.classList.remove("open")
+        arrow_down_feat_mob.classList.add("open")
+        arrow_up_feat_mob.classList.remove("open")
         is_open_feature_mob = false
     }
 };
 
+
+// Opening/closing sub-navigation bar for feature company
+const company_mobile = document.querySelector("#mobile-company");
+const wrapper_company_sub_mobile = document.querySelector("#company-nav-mobile");
+const arrow_down_comp_mob = document.querySelector("#arrow-down-comp-mob")
+const arrow_up_comp_mob = document.querySelector("#arrow-up-comp-mob")
+company_mobile.addEventListener("click", open_close_company_mobile);
+
+function open_close_company_mobile(){
+    if(is_open_company_mob == false){
+        wrapper_company_sub_mobile.classList.add("open")
+        arrow_down_comp_mob.classList.remove("open")
+        arrow_up_comp_mob.classList.add("open")
+        is_open_company_mob = true
+    }else{
+        wrapper_company_sub_mobile.classList.remove("open")
+        arrow_down_comp_mob.classList.add("open")
+        arrow_up_comp_mob.classList.remove("open")
+        is_open_company_mob = false
+    }
+};
